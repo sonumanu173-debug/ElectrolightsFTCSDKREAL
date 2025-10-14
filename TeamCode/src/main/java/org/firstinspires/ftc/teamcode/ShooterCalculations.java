594bc5fc9b8ed582872652c2c0ff21b9d4db6903
AyushRG10 <ageddamuri@gmail.com>
2025-10-14 18:23:21 -0500

    Created a class for shooter calculations
    work in progress.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterCalculations.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.lang.Math;
+
+public class ShooterCalculations {
+
+    //create a new class
+    AprilTagDistance tagDistance = new AprilTagDistance();
+    double distance = tagDistance.distance;
+    double angle = 63.2;
+
+}
