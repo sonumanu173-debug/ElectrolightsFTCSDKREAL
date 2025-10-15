13c8f09762559e3f2e21d9b52f70ffd399c198cf
AyushRG10 <ageddamuri@gmail.com>
2025-10-14 19:03:00 -0500

    TeleOp Mechanum Drive
    I coded the tele-op mechanum drive stuff, so as soon as we build the robot, we can start test driving this hopefully.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumFieldOrientatedOpMode.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MechanumDrive;
+
+public class MechanumFieldOrientatedOpMode extends OpMode {
+
+    MechanumDrive drive = new MechanumDrive();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        drive.init();
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward, strafe, rotate);
+    }
+
+}
+
