61ad5af17ba930fd8826e08331cd38ac64bc1d85
AyushRG10 <ageddamuri@gmail.com>
2025-10-15 21:06:51 -0500

    added random stuff
    blah stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+@TeleOp
 public class MechanumDrive {
 
     private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
