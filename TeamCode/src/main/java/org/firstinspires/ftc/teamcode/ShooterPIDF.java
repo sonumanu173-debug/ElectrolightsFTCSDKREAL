819ce891755d78940da135ea187307250d58fd57
AyushRG10 <ageddamuri@gmail.com>
2025-10-15 20:50:36 -0500

    PIDF
    needs work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterPIDF.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class ShooterPIDF extends LinearOpMode {
+
+    DcMotorEx motor;
+
+    public void runOpMode() throws InterruptedException {
+        motor = hardwareMap.get(DcMotorEx.class, "motor"); //motor has to be name in control hub
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
+
+    public double PIDControl(double reference, double state) {
+        
+    }
+
+}
