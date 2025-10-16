e8bf21576145745d8f028d1e80bc90485e1a4cab
AyushRG10 <ageddamuri@gmail.com>
2025-10-15 20:29:07 -0500

    ShooterCalculations finished
    need to update return function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterCalculations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterCalculations.java
@@ -1,12 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import java.lang.Math;
 
-public class ShooterCalculations {
+public class ShooterCalculations extends OpMode {
+
 
     //create a new class
     AprilTagDistance tagDistance = new AprilTagDistance();
-    double distance = tagDistance.distance;
-    double angle = 63.2;
+    double distance;
+    double initalVelocity;
+    double numerator;
+    double denominator;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        distance = tagDistance.distance;
+        numerator = Math.sqrt(9.81 * Math.pow(distance, 2));
+        denominator = (Math.pow(2 * Math.cos(63.2)) * (distance * Math.tan(63.2) - 0.85125));
+        initalVelocity = numerator / denominator;
+        return initalVelocity;
+    }
 
 }
