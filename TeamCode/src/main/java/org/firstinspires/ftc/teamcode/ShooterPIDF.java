0f866254f4ff0be641699f01e6a3e4e909c2e1b7
AxeLotlol <mokshnot5@gmail.com>
2025-10-16 16:54:50 -0500

    Update ShooterPIDF.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterPIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterPIDF.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+@Configurable
 public class ShooterPIDF extends LinearOpMode {
 
     DcMotorEx motor;
