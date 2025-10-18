e2db757ca0c350790d9fc4be9e05caeca8d1bbcf
AyushRG10 <ageddamuri@gmail.com>
2025-10-17 19:33:27 -0500

    stuff
    stuff hopefully not errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBench.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+
+public class TestBench {
+    private IMU imu;
+
+    public TestBench() {
+
+    }
+
+    public void init(HardwareMap hardwareMap) {
+        imu = hardwareMap.get(IMU.class, "imu");
+
+    }
+
+    public YawPitchRollAngles getOrientation() {
+        if (imu != null) {
+            return imu.getRobotYawPitchRollAngles();
+        } else {
+            return imu.getRobotYawPitchRollAngles();
+        }
+    }
+}
