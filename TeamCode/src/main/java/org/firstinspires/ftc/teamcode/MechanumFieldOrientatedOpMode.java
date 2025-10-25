9b0bf54c25f387238f002d2cb0d90589489808b5
AxeLotlol <mokshnot5@gmail.com>
2025-10-25 13:22:03 -0500

    Update MechanumFieldOrientatedOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumFieldOrientatedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumFieldOrientatedOpMode.java
@@ -1,29 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.mechanisms.MechanumDrive;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
 
-@TeleOp
-public class MechanumFieldOrientatedOpMode extends OpMode {
+@TeleOp(name = "NextFTC TeleOp Program Java")
+public class MechanumFieldOrientatedOpMode extends NextFTCOpMode {
+    public MechanumFieldOrientatedOpMode() {
+        addComponents(
 
-    MechanumDrive drive = new MechanumDrive();
-    double forward, strafe, rotate;
-
-    @Override
-    public void init() {
-        //drive.init();
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
     }
 
+    // change the names and directions to suit your robot
+    private final MotorEx frontLeftMotor = new MotorEx("front_left").reversed();
+    private final MotorEx frontRightMotor = new MotorEx("front_right");
+    private final MotorEx backLeftMotor = new MotorEx("back_left").reversed();
+    private final MotorEx backRightMotor = new MotorEx("back_right");
+
     @Override
-    public void loop() {
-        forward = gamepad1.left_stick_y;
-        strafe = gamepad1.left_stick_x;
-        rotate = gamepad1.right_stick_x;
-
-        drive.driveFieldRelative(forward, strafe, rotate);
+    public void onStartButtonPressed() {
+        Command driverControlled = new MecanumDriverControlled(
+                frontLeftMotor,
+                frontRightMotor,
+                backLeftMotor,
+                backRightMotor,
+                Gamepads.gamepad1().leftStickY().negate(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX()
+        );
+        driverControlled.schedule();
     }
-
 }
-
