15e55b2d3a6664f64a9fe453eb5a86386c2ef30d
sonumanu173-debug <sonumanu173@gmail.com>
2025-11-02 11:16:34 -0600

    Added field centric and robot centric code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name = "Autonomous Test (NextFTC)", group = "Autonomous")
-public class Autotest extends NextFTCOpMode {
+public class MainAuto extends NextFTCOpMode {
 
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
