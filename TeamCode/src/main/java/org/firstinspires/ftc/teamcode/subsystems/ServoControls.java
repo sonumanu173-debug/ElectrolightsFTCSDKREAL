3a89be09623cfb695768fcd57bdc64ad7724bd1b
ray090-commits <raymondy20110714@gmail.com>
2025-11-02 20:43:12 -0600

    ye

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ServoControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ServoControls.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.launchertestservo;
 
-
+@TeleOp(name = "Servo Test", group = "TeleOp")
 public class ServoControls extends OpMode{
     launchertestservo bench = new launchertestservo();
     @Override
@@ -12,11 +13,27 @@ public class ServoControls extends OpMode{
         bench.init(hardwareMap);
     }
 
-
-
     @Override
     public void loop() {
-        bench.setServoPos(-1.0);
+        if (gamepad1.a) {
+            bench.setServoPos(-1.0);
+        }
+
+        else if (gamepad1.b) {
+            bench.setServoPos(1.0);
+         }
+        else {
+            bench.setServoPos(0.0);
+        }
+
+
+
+
+
+
+
+
+
 
 
     }
