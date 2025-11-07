68fc80a0765e688b5f075d8b3ab815e073a56073
sonumanu173-debug <sonumanu173@gmail.com>
2025-11-06 22:08:52 -0600

    Converted ayushs code to a subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotifScanning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotifScanning.java
@@ -1,30 +1,38 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+
+
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.NextFTCOpMode;
 
-public class MotifScanning extends NextFTCOpMode {
 
+
+public class MotifScanning implements Subsystem{
     private Limelight3A limelight;
     LLResult llResult;
+    private HardwareMap hardwareMap;
+    public MotifScanning(HardwareMap hardwareMap){
+        this.hardwareMap = hardwareMap;
+    }
+
     public int tagID;
 
     @Override
-    public void onInit() {
+    public void initialize(){
+        MotifScanning motifScanning = new MotifScanning(hardwareMap);
+        motifScanning.initialize();
+
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-    }
-
-    public void onStartButtonPressed() {
         limelight.start();
+
     }
 
-    @Override
-    public void onUpdate() {
-        llResult = limelight.getLatestResult();
-    }
-
-    public int Motif() {
+    public int motif(){
+        tagID = -1;
         limelight.pipelineSwitch(1);
         if (llResult.isValid() && llResult != null) {
             tagID = 23;
@@ -38,11 +46,11 @@ public class MotifScanning extends NextFTCOpMode {
             tagID = 21;
         }
         return tagID;
+
     }
 
-    @Override
-    public void onStop() {
-        limelight.stop();
+    public void periodic(){
+        llResult = limelight.getLatestResult();
     }
 
 }
