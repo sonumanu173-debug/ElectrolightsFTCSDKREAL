357bc3d3695aaccb9e34179db5d2da482d287794
sonumanu173-debug <sonumanu173@gmail.com>
2025-11-08 08:42:59 -0600

    ESSENTIALLY FINISHED AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
@@ -1,4 +1,8 @@
+
+
 package org.firstinspires.ftc.teamcode;
+import java.util.concurrent.TimeUnit;
+
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
@@ -11,6 +15,7 @@ import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.impl.MotorEx;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
@@ -20,6 +25,8 @@ import org.firstinspires.ftc.teamcode.subsystems.MotifScanning;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.bylazar.configurables.annotations.Configurable;
+
+
 
 @Autonomous(name = "Autonomous Test (NextFTC)", group = "Autonomous")
 @Configurable
@@ -37,14 +44,43 @@ public class AutoBlue extends NextFTCOpMode {
     private int pathState;
     private Paths paths;
     public Pose start = new Pose(28,120, Math.toRadians(110));
+
     public Pose PreloadLaunch = new Pose(54.871, 97.258, Math.toRadians(130));
+
+    public Pose controlPoint1 = new Pose(50.263, 76.015);
+
+    public Pose intake1 = new Pose(6.963, 81.9);
+
+    public Pose ControlPose2 = new Pose(2.8484, 88.932);
+    public Pose ControlPose3 = new Pose(3.48132, 113.934);
+    public Pose Launch1 = new Pose(56.967033, 94);// THIS will be used for launching all 3 times lol
+
+    public Pose intake2ControlPose = new Pose(84.5011, 58.5495);
+
+    public Pose intake2 = new Pose(9.811, 59.499);
+
+    public Pose Intake3ControlPoint = new Pose(85.1341, 25.635165);
+
+    public Pose Intake3 = new Pose(9.81099, 35.4462);
+
+
+
+
+
+
+
+
+    private MotorEx intakeMotor;
+
+    private MotorEx spindexerMotor;
 
     public void onInit() {
         telemetry.addLine("Initializing Follower...");
         telemetry.update();
         follower = Constants.createFollower(hardwareMap);
         paths = new Paths(follower);
-
+        intakeMotor = new MotorEx("intake");
+        spindexerMotor = new MotorEx("spindexerMotor");
         pathTimer = new Timer();
         actionTimer = new Timer();
         opmodeTimer = new Timer();
@@ -62,6 +98,8 @@ public class AutoBlue extends NextFTCOpMode {
         pathTimer.resetTimer();
         int tag=MotifScanning.INSTANCE.findMotif();
         follower.followPath(paths.Path1);
+        Flywheel.shooter(1500);
+        telemetry.addLine("The shooter has started btw");
         telemetry.addLine("Started Path 1");
         telemetry.update();
     }
@@ -76,12 +114,28 @@ public class AutoBlue extends NextFTCOpMode {
                     // WIhs follower.followPath(paths.Path2);
 
                     telemetry.addLine("Path 1 has been completed btw, that means it's going to launch in a couple of seconds");
+
+
+                    intakeMotor.setPower(-1);
+                    telemetry.addLine("The intake has started btw");
+                    spindexerMotor.setPower(-1);
+                    telemetry.addLine("The spindexer motor has started btw");
+                    telemetry.update();
+                    follower.followPath(paths.Path2);
+                    telemetry.addLine("Started path 2 to intake the balls");
                     pathState++;
                 }
                 break;
             case 1:
                 if (!follower.isBusy()) {
-                    telemetry.addLine("Autonomous Complete LOLLLL");
+                    pathTimer.resetTimer();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    telemetry.addLine("Moving onto path 3 momentarily");
                     pathState++;
                 }
                 break;
@@ -103,6 +157,16 @@ public class AutoBlue extends NextFTCOpMode {
         public PathChain Path1;
         public PathChain Path2;
 
+        public PathChain Path3;
+
+        public PathChain Path4;
+
+        public PathChain Path5;
+
+        public PathChain Path6;
+
+        public PathChain Path7;
+
         public Paths(Follower follower) {
             //Path1 = follower.pathBuilder()
             //.addPath(new BezierLine(
@@ -115,10 +179,80 @@ public class AutoBlue extends NextFTCOpMode {
             Path1 = follower.pathBuilder()
                     .addPath(new BezierLine(
                             start,
+
                             PreloadLaunch
                     ))
                     .setTangentHeadingInterpolation()
+
                     .build();
+            Path2 = follower.pathBuilder()
+                    .addPath(new BezierCurve(
+                            PreloadLaunch,
+
+                            controlPoint1,
+
+                            intake1
+
+                    ))
+                    .setLinearHeadingInterpolation(Math.toRadians(175), Math.toRadians(180))
+
+                    .build();
+            Path3 = follower.pathBuilder()
+                    .addPath(new BezierCurve(
+                            intake1,
+
+                            ControlPose2,
+
+                            ControlPose3,
+
+                            Launch1
+
+
+                    ))
+                    .setConstantHeadingInterpolation(Math.toRadians(130))
+
+                    .build();
+            Path4 = follower.pathBuilder()
+                    .addPath(new BezierCurve(
+                            Launch1,
+
+                            intake2ControlPose,
+
+                            intake2
+                    ))
+                    .setLinearHeadingInterpolation(Math.toRadians(175), Math.toRadians(180))
+
+                    .build();
+            Path5 = follower.pathBuilder()
+                    .addPath(new BezierLine(
+                            intake2,
+
+                            Launch1
+                    ))
+                    .setConstantHeadingInterpolation(Math.toRadians(130))
+
+                    .build();
+            Path6 = follower.pathBuilder()
+                    .addPath(new BezierCurve(
+                            Launch1,
+
+                            Intake3ControlPoint,
+
+                            Intake3
+                    ))
+                    .setLinearHeadingInterpolation(Math.toRadians(175), Math.toRadians(180))
+
+                    .build();
+            Path7 = follower.pathBuilder()
+                    .addPath(new BezierCurve(
+                            Intake3,
+
+                            Launch1
+                    ))
+                    .setConstantHeadingInterpolation(Math.toRadians(130))
+
+                    .build();
+
         }
     }
 }
