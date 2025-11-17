1c1ef605942ac018fb52964ab4ffe0f640d3301f
AyushRG10 <ageddamuri@gmail.com>
2025-11-16 19:38:36 -0600

    blH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AprilTagDistanceEquation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AprilTagDistanceEquation.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
@@ -9,19 +10,21 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import dev.nextftc.ftc.NextFTCOpMode;
 
+//Use to create the distance equation
+@TeleOp
 public class AprilTagDistanceEquation extends NextFTCOpMode {
 
     TestBench bench = new TestBench();
 
     private Limelight3A limelight;
 
-    public double distance;
+    public double distance = 0;
 
     @Override
     public void onInit() {
         bench.init(hardwareMap);
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(0); //use 0 for distance calculations
+        limelight.pipelineSwitch(1); //use 0 for distance calculations
     }
 
     @Override
